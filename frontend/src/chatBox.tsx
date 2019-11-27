2013a6bee50ac3b970adcf1247d5443a8517a849
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-11-27 15:29:06 +0100

    created a chatBox component

---
+++ frontend/src/chatBox.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import io from 'socket.io-client';
+
+interface State {
+  username: string;
+  message: string;
+  messages: any;
+  returnMessages:any
+}
+
+interface Props {
+
+}
+
+export default class ChatBox extends React.Component<Props, State> {
+    constructor(props: Props){
+    super(props);
+
+    this.state = {
+        username: '',
+        message: '',
+        messages: [],
+        returnMessages:''
+    };  
+
+    this.sendMessage = this.sendMessage.bind(this);
+    
+        var socket = io('localhost:5000');
+        socket.on('RECEIVE_MESSAGE',(data: any)=>{
+            this.addMessage(data)
+            //this.setState({messages: data})
+            console.log(this.state.messages)
+            
+            //this.setState({messages: data});
+        /*socket.on('RECEIVE_MESSAGE', function(data: any) {
+            return(
+                <div>
+                    <h1>{data}</h1>
+                </div>
+            )*/
+        })
+
+};
+addMessage(data:any) {
+    console.log(data, 'im alive');
+    this.setState({messages: data});
+    console.log(this.state.messages);
+};
+
+sendMessage (event: React.FormEvent) {
+    var socket = io('localhost:5000');
+    event.preventDefault();
+    let msgs = this.state.messages;
+    msgs.push(this.state.message);
+    socket.emit('SEND_MESSAGE',msgs);
+    this.setState({message: ''});
+}
+
+showMessage = () => {
+    var socket = io('localhost:5000');
+    socket.on('RECEIVE_MESSAGE', (data: any)=>{
+        this.setState({returnMessages: data})
+    })
+
+    if (this.state.returnMessages.length > 0) {
+        return this.state.returnMessages.map((msg: String)=>{
+            return <li>{msg}</li>
+        })
+    }
+}
+
+/*addMessage() {
+    var socket = io('localhost:5000');
+    socket.on('RECEIVE_MESSAGE',(data:[])=>{
+        this.setState({messages: data});
+    socket.on('RECEIVE_MESSAGE', function(data: any) {
+        return(
+            <div>
+                <h1>{data}</h1>
+            </div>
+        )
+    })
+}*/
+
+render() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="card-title">Global Chat</div>
+                            <hr/>
+                            <div className="messages">
+                                <ul>
+                                    {this.showMessage()}
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="footer">
+                            <input type="text" placeholder="Username" className="form-control" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} />
+                            <br/>
+                            <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+                            <br/>
+                            <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )}
+}
