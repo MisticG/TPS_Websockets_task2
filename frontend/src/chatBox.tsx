99435e4cbebdd9b0722263464a61e6ac1b0c6f05
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-11-27 22:15:12 +0100

    clean up in chatBox

--- frontend/src/chatBox.tsx
+++ frontend/src/chatBox.tsx
@@ -25,56 +25,56 @@ export default class ChatBox extends React.Component<Props, State> {
 
     this.sendMessage = this.sendMessage.bind(this);
 
-};
+    };
 
-sendMessage (event: React.FormEvent) {
-    var socket = io('localhost:5000');
-    event.preventDefault();
-    let msgs = this.state.messages;
-    msgs.push(this.state.message);
-    socket.emit('SEND_MESSAGE',msgs);
-    this.setState({message: ''});
-}
+    private socket: any = io('localhost:5000');
 
-socket: any = io('localhost:5000');
-
-showMessage = () => {
-    this.socket.on('RECEIVE_MESSAGE', (data: any)=>{
-        this.setState({returnMessages: data})
-    })
-
-    if (this.state.returnMessages.length > 0) {
-        return this.state.returnMessages.map((msg: String)=>{
-            return <li>{msg}</li>
-        })
+    sendMessage (event: React.FormEvent) {
+        event.preventDefault();
+        let msgs = this.state.messages;
+        msgs.push(this.state.message);
+        this.socket.emit('SEND_MESSAGE',msgs);
+        this.setState({message: ''});
     }
-}
 
-render() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-4">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title">Global Chat</div>
-                            <hr/>
-                            <div className="messages">
-                                <ul>
-                                   {this.showMessage()}
-                                </ul>
+    showMessage = () => {
+        this.socket.on('RECEIVE_MESSAGE', (data: any)=>{
+            this.setState({returnMessages: data})
+        })
+
+        if (this.state.returnMessages.length > 0) {
+            return this.state.returnMessages.map((msg: String)=>{
+                return <li>{msg}</li>
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="card-title">Global Chat</div>
+                                <hr/>
+                                <div className="messages">
+                                    <ul>
+                                    {this.showMessage()}
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                        <div className="footer">
-                            <input type="text" placeholder="Username" className="form-control" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} />
-                            <br/>
-                            <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
-                            <br/>
-                            <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
+                            <div className="footer">
+                                <input type="text" placeholder="Username" className="form-control" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} />
+                                <br/>
+                                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+                                <br/>
+                                <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )}
+        )
+    }
 }
