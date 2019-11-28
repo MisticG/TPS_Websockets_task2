85d7a0b192655c5936ad0c3ef8947757672e7a4d
sakine.mazlomyar@bilvision.se <mazlomyar12@gmail.com>
2019-11-28 11:07:21 +0100

    added send message

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
+ 
 
 app.get('/',(req,res)=>{
     res.send('Hejjj')
-})
+})  
 
+let messages = []  
+//io is the connections objet to all clientsand socket is one single connection  
 io.on('connection',(socket)=>{
-    socket.emit('hello','World')
+
+    io.emit('message-history',messages)
+    
+    socket.on('single-message',(msg)=>{
+        messages.push(msg)
+        io.emit('single-message',msg)
+    })
+    
 })
 
 const server = http.listen(5000, ()=>{
