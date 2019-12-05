286ec532d3faf04a42206f1c301f14f60e9aa5e8
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-12-05 22:00:58 +0100

    conflict merged

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,14 @@
-import React, { Component } from 'react';
-//import Form from './Form';
-import Login from './Login';
-import io from 'socket.io-client';
-interface Props {
+import React, {Component} from 'react';
+import Form from './Form'
+import AutoComplete from './Autocomplete'
 
-}
-interface State {
-  rooms:string[]
-}
-export default class App extends Component<Props, State> {
-  private socket:SocketIOClient.Socket
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      rooms:[]
+export default class App extends Component {
+    render() {
+        return (
+              <div>
+                  <Form/>
+                  <AutoComplete />
+              </div>
+        )
     }
-
-    this.socket = io('http://localhost:5000');
-  }
-
-
-  getCurrentUser =(currentUser:{username:String, login:Boolean})=>{
-    console.log(currentUser) 
-  }
-
-  componentDidMount() {
-    this.socket.on('rooms',(rooms:string[])=>{
-         this.setState({rooms:rooms})
-    })
-  }
-  
-
-  render() {
-    return (
-      <div className="container-fluid" style={{display: 'flex', justifyContent: "center", marginTop: "5em"}}>
-        <div className="shadow border border-light rounded">
-          <Login getCurrentUser={this.getCurrentUser} rooms={this.state.rooms}/>
-        </div>
-      </div>
-    );
-  }
-};
+}
